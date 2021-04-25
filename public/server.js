184d23c494578beb2cf1ea2f5fd39cfd4705b395
mcraet <mcraeteisha@gmail.com>
2021-04-25 00:44:57 -0400

    adding package.json, node modules, and gitignore

--- public/server.js
+++ public/server.js
@@ -8,11 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+
+require('/routes/apiRoutes.js')(app);
+require('/routes/htmlRoutes.js')(app);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
